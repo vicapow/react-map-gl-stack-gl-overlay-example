e5d2230d5b4099dbd243e1003f40f6b24859e247
Victor Powell <vp@uber.com>
2015-11-29 21:20:13 -0800

    Dynamically scale points but no smaller than one device pixel.

--- main.fragment.glsl
+++ main.fragment.glsl
@@ -4,8 +4,8 @@ varying vec4 fragColor;
 
 void main() {
   // Uncommon to draw points as circles instead of squares.
-  // if (distance(gl_PointCoord.st, vec2(0.5, 0.5)) > 0.5) {
-  //   discard;
-  // }
+  if (distance(gl_PointCoord.st, vec2(0.5, 0.5)) > 0.5) {
+    discard;
+  }
   gl_FragColor = fragColor;
 }
