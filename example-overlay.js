906db3a111c2686a476de585f45ddaa4de9c0056
Victor Powell <vp@uber.com>
2015-11-29 16:03:22 -0800

    Add a working demo of the component.

---
+++ example-overlay.js
@@ -0,0 +1,165 @@
+'use strict';
+
+var React = require('react/addons');
+var PureRenderMixin = React.addons.PureRenderMixin;
+var window = require('global/window');
+var r = require('r-dom');
+var getContext = require('get-canvas-context');
+var glslify = require('glslify');
+var createShader = require('gl-shader');
+var createVAO = require('gl-vao');
+var createBuffer = require('gl-buffer');
+
+var PI = Math.PI;
+var DEGREES_TO_RADIANS = PI / 180;
+
+function radians(degrees) {
+  return degrees * DEGREES_TO_RADIANS;
+}
+
+// Convert a JavaScript double precision Number to the equivalent single
+// precision value.
+function float(value) {
+  var array = new Float32Array(1);
+  array[0] = value;
+  return array[0];
+}
+
+// Split a Number of double precision into two Numbers of single precision.
+function split(a) {
+  var tHi = float(a);
+  var tLo = a - tHi;
+  return [tHi, tLo];
+}
+
+var CanvasOverlay = React.createClass({
+
+  displayName: 'StackGLOverlay',
+
+  mixins: [PureRenderMixin],
+
+  propTypes: {
+    width: React.PropTypes.number,
+    height: React.PropTypes.number,
+    zoom: React.PropTypes.number.isRequired,
+    latitude: React.PropTypes.number.isRequired,
+    longitude: React.PropTypes.number.isRequired,
+    isDragging: React.PropTypes.bool,
+    locations: React.PropTypes.array.isRequired,
+    lngLatAccessor: React.PropTypes.func.isRequired
+  },
+
+  getDefaultProps: function getDefaultProps() {
+    return {
+      lngLatAccessor: function defaultLocationAccessor(location) {
+        return location;
+      }
+    };
+  },
+
+  componentWillReceiveProps: function componentWillReceiveProps(nextProps) {
+    if (nextProps.locations !== this.props.locations) {
+      this._updateVAO();
+    }
+  },
+
+  _getLocationsBufferArray: function _getLocationsBufferArray() {
+    var ret = [];
+    var locations = this.props.locations || [];
+    var location;
+    var x;
+    var y;
+    var phi;
+    var lamda;
+    var xHiLo;
+    var yHiLo;
+    for (var i = 0; i < locations.length; i++) {
+      location = this.props.lngLatAccessor(locations[i]);
+      lamda = radians(location[0]);
+      phi = radians(location[1]);
+      // [0, 1]
+      x = (lamda + Math.PI) / Math.PI * 0.5;
+      // [0, 1]
+      y = (PI + Math.log(Math.tan(PI * 0.25 + phi * 0.5))) / Math.PI * 0.5;
+      xHiLo = split(x);
+      yHiLo = split(y);
+      ret.push(xHiLo[0]);
+      ret.push(xHiLo[1]);
+      ret.push(yHiLo[0]);
+      ret.push(yHiLo[1]);
+    }
+    return ret;
+  },
+
+  _updateVAO: function _updateVAO() {
+    var gl = this._ctx;
+    if (this._vao) {
+      this._vao.dispose();
+    }
+    this._vao = createVAO(gl, [{
+      buffer: createBuffer(gl, this._getLocationsBufferArray()),
+      type: gl.FLOAT,
+      size: 4
+    }]);
+  },
+
+  componentDidMount: function componentDidMount() {
+    var gl = this._ctx = getContext('webgl', {canvas: this.getDOMNode()});
+    this._shader = createShader(gl,
+      glslify('./main.vertex.glsl'),
+      glslify('./main.fragment.glsl'));
+    this._shader.attributes.coordinate.location = 0;
+    this._updateVAO();
+    this._redraw();
+  },
+
+  componentDidUpdate: function componentDidUpdate() {
+    this._redraw();
+  },
+
+  _redraw: function _redraw() {
+    var gl = this._ctx;
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.blendFunc(gl.ONE, gl.ONE);
+    gl.enable(gl.BLEND);
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    this._shader.bind();
+    var lnglat = [this.props.longitude, this.props.latitude];
+    var zoom = this.props.zoom;
+    var tileSize = 512;
+    var scale = tileSize * Math.pow(2, zoom);
+    var lambda = radians(lnglat[0]);
+    var phi = radians(lnglat[1]);
+    var x = (lambda + Math.PI) / Math.PI * 0.5;
+    var y = (PI + Math.log(Math.tan(PI * 0.25 + phi * 0.5))) / Math.PI * 0.5;
+    this._shader.uniforms.alpha = 0.1;
+    this._shader.uniforms.pointSize = 16 * Math.pow(2, zoom - 20);
+    this._shader.uniforms.scale = split(scale);
+    this._shader.uniforms.center = split(x).concat(split(y));
+    this._shader.uniforms.dimensions = [this.props.width, this.props.height];
+    // this._shader.uniforms.color = [31 / 255, 186 / 255, 214 / 255, 1];
+    this._shader.uniforms.color = [231 / 255, 76 / 255, 60 / 255, 1];
+    this._vao.bind();
+    this._vao.draw(gl.POINTS, this.props.locations.length);
+    this._vao.unbind();
+  },
+
+  render: function render() {
+    var pixelRatio = window.devicePixelRatio || 1;
+    return r.canvas({
+      ref: 'overlay',
+      width: this.props.width * pixelRatio,
+      height: this.props.height * pixelRatio,
+      style: {
+        width: this.props.width + 'px',
+        height: this.props.height + 'px',
+        position: 'absolute',
+        pointerEvents: 'none',
+        left: 0,
+        top: 0
+      }
+    });
+  }
+});
+
+module.exports = CanvasOverlay;
---
+++ index.js
@@ -0,0 +1,86 @@
+'use strict';
+
+var document = require('global/document');
+var window = require('global/window');
+var React = require('react');
+var r = require('r-dom');
+var MapGL = require('react-map-gl');
+var process = require('global/process');
+var StackGLOverlay = require('react-map-gl-stack-gl-overlay');
+var assign = require('object-assign');
+var d3 = require('d3');
+
+// var locations = require('example-cities');
+
+// This will get converted to a string by envify
+/* eslint-disable no-process-env */
+var mapboxApiAccessToken = process.env.MapboxAccessToken;
+/* eslint-enable no-process-env */
+
+var App = React.createClass({
+
+  displayName: 'App',
+
+  getInitialState: function getInitialState() {
+    return {
+      appWidth: window.innerWidth,
+      appHeight: window.innerHeight,
+      viewport: {
+        latitude: 40.71415,
+        longitude: -74.006,
+        zoom: 20
+      }
+    };
+  },
+
+  componentDidMount: function componentDidMount() {
+    window.addEventListener('resize', function onResize() {
+      this.setState({
+        appWidth: window.innerWidth,
+        appHeight: window.innerHeight
+      });
+    }.bind(this));
+    d3.csv('/picks.csv', function accessor(row) {
+      return [Number(row.longitude), Number(row.latitude)];
+    }, function response(error, trips) {
+      if (error) {
+        throw error;
+      }
+      // trips = [
+      //   [-90, 0],
+      //   [90, 0],
+      //   [0, 45],
+      //   [-90, 45],
+      //   [90, 45],
+      //   [0, 0]
+      // ];
+      // trips = trips.slice(0, 10000);
+      this.setState({trips: trips});
+    }.bind(this));
+  },
+
+  _onChangeViewport: function _onChangeViewport(viewport) {
+    viewport.zoom = d3.round(viewport.zoom, 4);
+    this.setState({viewport: viewport});
+  },
+
+  render: function render() {
+    var props = assign({}, this.state.viewport, {
+      width: this.state.appWidth,
+      height: this.state.appHeight,
+      mapboxApiAccessToken: mapboxApiAccessToken,
+      onChangeViewport: this._onChangeViewport
+    });
+    return r(MapGL, props, [
+      this.state.trips ? r(StackGLOverlay, {
+        locations: this.state.trips,
+        lngLatAccessor: this._tripLngLatAccessor,
+        zoom: this.state.viewport.zoom,
+        longitude: this.state.viewport.longitude,
+        latitude: this.state.viewport.latitude
+      }) : null
+    ]);
+  }
+});
+document.body.style.margin = 0;
+React.render(r(App), document.body);
